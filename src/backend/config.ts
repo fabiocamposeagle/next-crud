f2f93a6d0b6282b48515f3983814cacf7d5438cb
Fábio Campos Developer <eaglebooks77@gmail.com>
2021-11-17 21:50:16 -0300

    Update config.ts

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -5,7 +5,7 @@ if (!firebase.apps.length) {
     firebase.initialiseApp({
         apikey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectid: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     })
 }
 
